7b44c7ebae6a882bee8f67ddba2d193e1c29dfed
Alexandra Macovei <macovei.a2@gmail.com>
2023-07-06 10:20:40 +0200

    created new componenet for storing the date

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,52 @@
-import { useState } from 'react';
 import ExpenseItem from './components/ExpenseItem';
-// import { ExpenseObj} from './Types';
-import React from 'react';
 
 function App() {
-  //making a hardcoded  expense array instead for the database
   const expenses = [
     {
       id: 'e1',
-      title: 'Dress',
-      amount: 1299,
-      date: new Date(2023, 6, 14),
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
     },
-    {
-      id: 'e2',
-      title: 'iphone Case',
-      amount: 499.99,
-      date: new Date(2023, 5, 12),
-    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Dog Insurance',
+      title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2023, 6, 28),
+      date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'Ice mobile',
+      title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2023, 7, 12),
+      date: new Date(2021, 5, 12),
     },
   ];
 
   return (
-    <>
-      <section>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date.toString()}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date.toString()}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date.toString()}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date.toString()}
-        ></ExpenseItem>
-      </section>
-    </>
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
   );
 }
 
--- frontend/src/components/ExpenseItem.tsx
+++ frontend/src/components/ExpenseItem.tsx
@@ -1,15 +1,14 @@
 import { ReactNode } from 'react';
 import './ExpenseItem.css'
-import { ExpenseObj } from '../Types';
+import ExpenseDate from './ExpenseDate';
 
-const expenseItem = (props:ExpenseObj)=>{
+const expenseItem = (props:any)=>{
     // creating and hard coding dummy data
-    // const expanseDate = new Date(2023, 3, 26).toString()
-    // const expenseTitle = 'Dog Insurance';
-    // const expensesAmount = 250;
+
+
     return(
         <section className='expense-item'>
-            <div>{props.date}</div>
+         <ExpenseDate date = {props.date}></ExpenseDate>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{`${props.amount},-`} </div>
